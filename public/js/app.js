a6efbd218ca7c5cc52533c7cb760d371772424db
Mickey_413 <43994883+ultimatecoder2@users.noreply.github.com>
2020-08-02 09:03:15 +0530

    Removed UnWanted Code

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-console.log("Hello there. I'm from JS file");
-
-
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
